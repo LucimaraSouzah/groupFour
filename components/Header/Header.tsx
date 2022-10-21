b759890e0d70feb270f3cf2be93bb666dcc32b81
Lucimara <lucimarasouzah@gmail.com>
2022-10-21 11:40:38 -0300

    Desafio

---
+++ components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <title>Projeto React - Trabalho em Grupo</title>
+      <meta
+        name="description"
+        content="Objetivo é desmembrar este arquivo em vários componentes"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
